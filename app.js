5dbbec64263e158fcf475a50e9c9e11e060e9a3f
Your Name <you@example.com>
2018-05-24 15:39:20 +0400

    asdf

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.listen(3000, function(){
 });
 
 var Grass = require("./class/class.grass");
-var EatGrass = require("./class/class.ekagrass");
+var EatGrass = require("./class/class.eatgrass");
 var Gishatich = require("./class/class.gishatich");
 var Valod = require("./class/class.valod");
 var ClassM = require("./class/class");
